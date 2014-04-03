9ccb1a551ef4999705cbca92416dce7067df7caf
Alexandru Dascalu <alexandru.dascalu94@gmail.com>
2014-04-03 14:44:26 +0300

    View pager

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Fragments in a ViewPager example.
+This code shows how to replace Android [Fragments](http://developer.android.com/guide/components/fragments.html) inside a [ViewPager](http://developer.android.com/reference/android/support/v4/view/ViewPager.html).
+
+Imagine the following structure:
+
+[ Root fragment | Side fragment ]
+
+You can replace _Root fragment_ for another one and, at any moment, you could swipe to _Side fragment_. In the code, I'm using <strong>support library v7</strong> ## Modified from this example: https://github.com/danilao/fragments-viewpager-example by Alexandru Dascalu: GooglePlus [https://plus.google.com/u/0/105620305121883379160/posts]  or GitHub: [https://github.com/alexandrudsc]
---
+++ src/com/alexandru/developer/replaceFragmentInViewPager/RootFragment.java
@@ -0,0 +1,53 @@
+﻿/*
+ * Copyright © 2014 Alexandru Dascălu.
+ * com.alex.independent.developer
+ */
+
+package com.alexandru.developer.replaceFragmentInViewPager;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+// Fake fragment used to define a container for the "real" fragment.
+
+public class RootFragment extends Fragment{
+
+
+    	private ViewGroup rootFragment;
+
+	@Override
+	public View onCreateView (LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState){
+        if(savedInstanceState!=null)
+            return (ViewGroup)this.getFragmentManager().getFragment(savedInstanceState, "root_index").getView();
+
+	rootFragment=(ViewGroup)inflater.inflate(R.layout.root_framgment, container, false);
+
+        FragmentTransaction transaction=this.getFragmentManager().beginTransaction();
+
+        RealFragment indexFragment=new RealFragment();
+
+        transaction.replace(R.id.root_frame1, indexFragment);
+
+        transaction.commit();
+
+		return rootFragment;
+	}
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+
+        //Save instance of fragment to use in onCreateView, in case onDestroyView was called
+        this.getFragmentManager().putFragment(outState, "root_index", this);
+    }
+
+}
